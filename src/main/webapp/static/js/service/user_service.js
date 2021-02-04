aafbcf56d714b8c2fda9718540c0f12e24067145
Aarju Sinha <49508889+OCTO01032020-araju@users.noreply.github.com>
2021-02-05 00:20:06 +0530

    Update user_service.js

--- src/main/webapp/static/js/service/user_service.js
+++ src/main/webapp/static/js/service/user_service.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp').factory('UserService', ['$http', '$q', function($http, $q){
 
-    var REST_SERVICE_URI = 'http://localhost:8080/Spring4MVCAngularJSExample/user/';
+    var REST_SERVICE_URI = 'http://localhost:9080/Spring4MVCAngularJSExample/user/';
 
     var factory = {
         fetchAllUsers: fetchAllUsers,
